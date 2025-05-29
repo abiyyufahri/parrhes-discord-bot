27270300ed332de445dde46ff83a2e15bc8cf785
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-05-29 17:30:51 +0700

    fix: some fix 3

--- src/config/bot.js
+++ src/config/bot.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  TOKEN: process.env.DISCORD_TOKEN || "MTE2NTIxNzkxMDIxMDQ0OTQzOA.Gftoqx.Kq65S3y1tDrpLzg0yWzc3QHXRZCdG3x1-0UDbs",
+  TOKEN: process.env.DISCORD_TOKEN || "",
   ownerID: [], //write your discord user id. example: ["id"] or ["id1","id2"]
   botInvite: "", //write your discord bot invite.
   supportServer: "", //write your discord bot support server invite.
